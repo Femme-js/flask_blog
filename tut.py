8772ec24811e777fe2fd02dd4d28f65ffb4e05fb
Femme-js <itsjeevu@gmail.com>
2020-04-24 17:39:34 +0530

    initial commit

---
+++ tut.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "its my landing page."
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+   
