a9f0d4f7e42136be0d7a31636b8989eacfc125cf
Femme-js <itsjeevu@gmail.com>
2020-05-01 19:28:40 +0530

    templating

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+    app.run(port=5000, debug=True)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+    app.run(port=5000, debug=True)
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
+    app.run(port=5000, debug=True)
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+    app.run(port=5000, debug=True)
+
+
+
+
